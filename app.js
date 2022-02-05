848735dbeb80f30068caaa9cf7344453acf427ce
Caleb Waymeyer <cjwaymeyer@gmail.com>
2022-02-05 09:54:45 -0500

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import express from "express";
+import cors from "cors";
 import { NotFoundError } from "./expressError.js";
 
 import authRoutes from "./routes/auth.js";
@@ -9,6 +10,7 @@ import goalsRoutes from "./routes/goals.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
